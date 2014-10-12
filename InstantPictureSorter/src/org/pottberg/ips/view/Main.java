16517ce220e6ef32e9db64af7095d29719221d40
Sören Pottberg <soerenpottberg@web.de>
2014-10-12 11:01:28 +0200

    Fix bug resulting from garbage collection

--- InstantPictureSorter/src/org/pottberg/ips/view/Main.java
+++ InstantPictureSorter/src/org/pottberg/ips/view/Main.java
@@ -9,11 +9,14 @@ import org.pottberg.ips.controller.MainController;
 
 public class Main extends BorderPane {
 
+    private MainController controller;
+
     public Main() {
 	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
 	    "Main.fxml"));
 	fxmlLoader.setRoot(this);
-	fxmlLoader.setController(new MainController());
+	controller = new MainController();
+	fxmlLoader.setController(controller);
 	try {
 	    fxmlLoader.load();
 	} catch (IOException exception) {
